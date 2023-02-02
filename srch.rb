47c15b4f3274d80e0f5d70792066576ddc6533b9
Roman Popa <rpopa.dp@gmail.com>
2023-02-02 23:02:15 +0200

    Refactor code

--- srch.rb
+++ srch.rb
@@ -1,130 +1,201 @@
 require 'pp'
 
-class Tokenizer
-  TOKEN_BEGIN = :token_begin
-  TOKEN_END = :token_end
+module Models
+  class Document
+    attr_reader :id, :body
 
-  def call(content)
-    Enumerator.new do |yielder|
-      start = 0
+    def initialize(id:, body:)
+      @id = id
+      @body = body
+    end
+  end
 
-      token_boundaries(content).each do |type, index|
-        case type
-        when TOKEN_BEGIN
-          start = index
-        when TOKEN_END
-          token = content[start..index]
+  class Entry
+    attr_reader :document, :term_freq
 
-          yielder.yield(token.downcase)
-        else
-          raise NotImplementedError, type
+    def initialize(document:, term_freq:)
+      @document = document
+      @term_freq = term_freq
+    end
+  end
+end
+
+class Commands
+  class Tokenize
+    TOKEN_BEGIN = :token_begin
+    TOKEN_END = :token_end
+
+    def call(content)
+      Enumerator.new do |yielder|
+        start = 0
+
+        token_boundaries(content).each do |type, index|
+          case type
+          when TOKEN_BEGIN
+            start = index
+          when TOKEN_END
+            token = content[start..index]
+
+            yielder.yield(token.downcase)
+          else
+            raise NotImplementedError, type
+          end
         end
       end
     end
-  end
 
-  private
+    private
 
-  def token_boundaries(content)
-    Enumerator.new do |yielder|
-      inside_token = false
+    def token_boundaries(content)
+      Enumerator.new do |yielder|
+        inside_token = false
 
-      content.each_char.with_index do |char, index|
-        if token_char?(char)
-          if !inside_token
-            yielder.yield(TOKEN_BEGIN, index)
-            inside_token = true
-          end
-        else
-          if inside_token
-            yielder.yield(TOKEN_END, index - 1)
-            inside_token = false
+        content.each_char.with_index do |char, index|
+          if token_char?(char)
+            if !inside_token
+              yielder.yield(TOKEN_BEGIN, index)
+              inside_token = true
+            end
+          else
+            if inside_token
+              yielder.yield(TOKEN_END, index - 1)
+              inside_token = false
+            end
           end
         end
-      end
 
-      if inside_token
-        yielder.yield(TOKEN_END, content.size - 1)
+        if inside_token
+          yielder.yield(TOKEN_END, content.size - 1)
+        end
+      end
+    end
+
+    def token_char?(char)
+      !whitespace?(char) && !punctuation?(char)
+    end
+
+    def whitespace?(char)
+      char == ' ' ||
+      char == "\r" ||
+      char == "\n" ||
+      char == "\t" ||
+      char == "\v" ||
+      char == "\f"
+    end
+
+    def punctuation?(char)
+      (33..47).include?(char.ord) ||
+      (58..64).include?(char.ord) ||
+      (91..96).include?(char.ord) ||
+      (123..126).include?(char.ord)
+    end
+  end
+
+  class IndexDirectory
+    def call(index, pattern)
+      Dir.glob(pattern).each do |file_path|
+        file_contents = File.read(file_path)
+        document = Models::Document.new(
+          id: file_path,
+          body: file_contents,
+        )
+
+        index.add(document)
       end
     end
   end
 
-  def token_char?(char)
-    !whitespace?(char) && !punctuation?(char)
-  end
-
-  def whitespace?(char)
-    char == ' ' ||
-    char == "\r" ||
-    char == "\n" ||
-    char == "\t" ||
-    char == "\v" ||
-    char == "\f"
-  end
-
-  def punctuation?(char)
-    (33..47).include?(char.ord) ||
-    (58..64).include?(char.ord) ||
-    (91..96).include?(char.ord) ||
-    (123..126).include?(char.ord)
-  end
-end
-
-def tf(doc, term)
-  count = doc[term]
-  sum = doc.values.sum
-
-  count.to_f / sum
-end
-
-def idf(docs, term)
-  total = docs.count
-  count = docs.count { |_, doc| doc.key?(term) }
-
-  if count.zero?
-    0.0
-  else
-    Math.log2(total.to_f / count)
-  end
-end
-
-def tf_idf(docs, doc_name, term)
-  a = tf(docs[doc_name], term)
-  b = idf(docs, term)
-
-  a * b
-end
-
-def query_docs(docs, query)
-  query_terms = Tokenizer.new.call(query).to_a
-
-  results = docs.keys.map do |doc_name|
-    rank = query_terms.sum do |term|
-      tf_idf(docs, doc_name, term)
+  class Search
+    def initialize(tokenize:)
+      @tokenize = tokenize
     end
 
-    [rank, doc_name]
+    def call(index, query)
+      query_terms = @tokenize.call(query).to_a
+
+      results = index.entries.map do |entry|
+        rank = query_terms.sum do |term|
+          tf_idf(index, entry, term)
+        end
+
+        [rank, entry.document.id]
+      end
+
+      results.sort!
+      results.reverse!
+
+      results
+    end
+
+    # TODO: cache `sum`
+    def tf(entry, term)
+      count = entry.term_freq[term]
+      sum = entry.term_freq.values.sum
+
+      count.to_f / sum
+    end
+
+    # TODO: cache `count`
+    def idf(index, term)
+      total = index.entries.count
+      count = index.entries.count { |entry| entry.term_freq.key?(term) }
+
+      if count.zero?
+        0.0
+      else
+        Math.log2(total.to_f / count)
+      end
+    end
+
+    def tf_idf(index, entry, term)
+      tf(entry, term) * idf(index, term)
+    end
   end
-
-  results.sort!
-  results.reverse!
-
-  results
 end
+
+module Repositories
+  class Index
+    attr_reader :entries
+
+    def initialize(tokenize:)
+      @tokenize = tokenize
+
+      @entries = []
+    end
+
+    def add(document)
+      term_freq = @tokenize.call(document.body).tally
+
+      @entries.push(
+        Models::Entry.new(
+          document: document,
+          term_freq: term_freq,
+        )
+      )
+    end
+  end
+end
+
 
 # ------------------------------------------------------------------------------
-docs = {}
+tokenize = Commands::Tokenize.new
+index = Repositories::Index.new(
+  tokenize: tokenize,
+)
+index_directory = Commands::IndexDirectory.new
+search = Commands::Search.new(
+  tokenize: tokenize,
+)
 
-Dir.glob('texts/*.txt').each do |file_path|
-  file_contents = File.read(file_path)
-  tokens = Tokenizer.new.call(file_contents)
-
-  docs[file_path] = tokens.tally
-end
+index_directory.call(index, 'texts/*.txt')
 
 loop do
   $stdout.puts('-' * 40)
   $stdout << 'query: '
 
-  pp query_docs(docs, $stdin.gets)
+  query = $stdin.gets
+
+  break unless query
+
+  pp search.call(index, query)
 end
